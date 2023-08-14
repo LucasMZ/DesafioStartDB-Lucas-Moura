a6dce39af17d635b57be6278952a2f6ff0c601ac
Lucas Moura <92659220+LucasMZ@users.noreply.github.com>
2023-08-14 19:20:31 -0300

    Add files via upload

--- caixa-da-lanchonete.js
+++ caixa-da-lanchonete.js
@@ -1,10 +1,19 @@
-const cardapio = {
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        const cardapio = {
     cafe: {
         codigo: "cafe",
         valor: 3.00
     },
-    chantilly:{
-        codigo: "chantilly",
+    chantily:{
+        codigo: "chantily",
         valor: 1.50
     },
     suco: {
@@ -31,37 +40,75 @@ const cardapio = {
     codigo: "combo2",
     valor: 7.5
     }
+}  
+const itens = ['cafe,1', 'chantily,1'];
+let pagamento = "credito";
+let soma = 0;
+let extra;
+const resultado = itens.map(function(separar) {
+    let [item, qtd] = separar.split(',');
+   
+    if(item.includes("queijo" || item.includes("chantilly" && item.includes(cardapio.item != queijo && chantily)))){
+        console.log("nao pode comprar sem o item principal")
+    }
+     qtd = parseInt(qtd);
+     let valorItem = cardapio[item].valor; 
+     return { item, qtd, valorItem };
+    
+});
+if(itens === null){
+    console.log("não há itens no carrinho")
 }
-class CaixaDaLanchonete {    
-    metodoDePagamento = "debito";
-    itens = ['combo1,1', 'chantily,1'];
-calcularValorDaCompra(metodoDePagamento, itens) {
-    if(itens == 0 || qtd.length == 0){
-        console.log("Não há itens no carrinho de compra!")
-    }
-    if(qtd.length == 0){
-        console.log("Quantidade inválida!");
-    }
-    if(!cardapio[item]){
-        console.log("Item inválido!");
-    }
-    if(!metodoDePagamento){
-        console.log("Forma de pagamento inválida")
-    }
-        resultado = itens.map(function(separar){
-            let [item, qtd] = separar.split(',');
-                    qtd = parseInt(qtd);
-                    total += qtd * cardapio[item].valor; // Acumular o valor total
-                    if(metodoDePagamento == "debito"){
-                        total -= total*0.05;
-                    }else if(metodoDePagamento == "credito"){
-                        total += total*0.03;
-                    }else if(metodoDePagamento == "dinheiro"){
-                        total -= total*0.05;
-                    }
-                    return { item, qtd };
-        })
-        return total.toFixed(2);
-    }
+if(itens.length === 0){
+    console.log("quantidade de itens invalida")
 }
-export { CaixaDaLanchonete };
+if(cardapios.codigo != item){
+    console.log("item invalido")
+}
+ soma = resultado.reduce(function(total, itemInfo) {
+    let valorItem = cardapio[itemInfo.item].valor * itemInfo.qtd;  // utiliza o array mapeado de resultados para multiplicar cada indice usando reduce
+    return total + valorItem;
+}, 0);
+if (pagamento !== "debito" && pagamento !== "credito" && pagamento !== "dinheiro") {
+        console.log("Forma de pagamento inválida!");
+    }
+if (pagamento === 'credito') {
+    soma += soma * 0.03;
+} else if (pagamento === 'dinheiro') {
+    soma -= soma * 0.05;
+}
+
+console.log("Resultado por item:", resultado);
+console.log("Total da compra:", soma.toFixed(2));
+
+
+
+
+
+/*     
+let total = [];
+let pagamento = "credito";
+let x = [];
+    const itens = ['cafe,1', 'chantily,1'];
+        const resultado = itens.map(function(separar){ // percorrendo o array
+            let [item, qtd] = separar.split(','); // desestruturando o array
+            qtd = parseInt(qtd); 
+
+            return {  item, qtd };
+       
+});
+*/
+
+        
+        
+        
+        
+     
+       
+        
+       
+        
+       
+    </script>
+</body>
+</html>
